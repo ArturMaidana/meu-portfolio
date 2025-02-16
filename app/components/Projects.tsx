fca5b1268e61513b274b7ef561a961cdb476529b
Artur Guilherme <132027060+ArturMaidana@users.noreply.github.com>
2025-02-16 00:28:14 -0400

    Ajustei as dimensões das imagens
    As imagens estavam numa dimensão muito grande oq estava dificultando na renderização

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -5,103 +5,100 @@ import { X } from "lucide-react";
 
 interface Project {
   id: number;
-  title: string;
-  description: string;
+  // title: string;
+  // description: string;
   image: string;
-  details: string[];
-  gallery: string[];
+  // details: string[];
+  // gallery: string[];
 }
 
 const projects: Project[] = [
   {
     id: 1,
-    title: "Defensoria Pública MT Cidadão - Mobile",
-    description:
-      "Acesse serviços e informações da Defensoria Pública de MT de forma rápida e fácil!\nSimplificamos o seu acesso à justiça com o app da Defensoria Pública de Mato Grosso. Consulte processos, agende atendimentos e obtenha orientações jurídicas diretamente pelo seu celular. Fácil de usar, mesmo para quem não é familiarizado com aplicativos.",
+    // title: "Defensoria Pública MT Cidadão - Mobile",
+    // description:
+    //   "Acesse serviços e informações da Defensoria Pública de MT de forma rápida e fácil!\nSimplificamos o seu acesso à justiça com o app da Defensoria Pública de Mato Grosso. Consulte processos, agende atendimentos e obtenha orientações jurídicas diretamente pelo seu celular. Fácil de usar, mesmo para quem não é familiarizado com aplicativos.",
     image: "/images/BannerCid.png",
-    details: [
-      "Solicitação e acompanhamento de agendamento.",
-      "Acompanhe seus processos.",
-      "Emissão de Hipossuficiência.",
-      "Canal de Ouvidoria",
-      "Encontre a Defensoria mais próxima de você",
-      "Fique por dentro das principais notícias",
-    ],
-    gallery: ["/images/BannerCid.png"],
+    // details: [
+    //   "Solicitação e acompanhamento de agendamento.",
+    //   "Acompanhe seus processos.",
+    //   "Emissão de Hipossuficiência.",
+    //   "Canal de Ouvidoria",
+    //   "Encontre a Defensoria mais próxima de você",
+    //   "Fique por dentro das principais notícias",
+    // ],
+    // gallery: ["/images/BannerCid.png"],
   },
   {
     id: 2,
-    title: "Assistente de Gabinete",
-    description:
-      "Desenvolvido para auxiliar os gabinetes da Defensoria Pública de Mato Grosso, o assistente de gabinete reúne ferramentas essenciais para otimizar o trabalho dos defensores e suas equipes. Com transcrição automatizada, inteligência artificial e consulta rápida de processos, o serviço facilita o acesso a informações e melhora a produtividade no dia a dia.",
+    // title: "Assistente de Gabinete",
+    // description:
+    //   "Desenvolvido para auxiliar os gabinetes da Defensoria Pública de Mato Grosso, o assistente de gabinete reúne ferramentas essenciais para otimizar o trabalho dos defensores e suas equipes. Com transcrição automatizada, inteligência artificial e consulta rápida de processos, o serviço facilita o acesso a informações e melhora a produtividade no dia a dia.",
     image: "/images/gabinete.png",
-    details: ["Transcrições", "IA para defensores", "Consulta de Processos"],
-    gallery: ["/images/01.png", "/images/02.png", "/images/03.png"],
+    // details: ["Transcrições", "IA para defensores", "Consulta de Processos"],
+    // gallery: ["/images/01.png", "/images/02.png", "/images/03.png"],
   },
   {
     id: 3,
-    title: "Portal Servidor Versão Mobile",
-    description:
-      "O Portal do Servidor ainda está em fase de protótipo, com novas funcionalidades sendo desenvolvidas para melhorar a experiência dos defensores e servidores da Defensoria Pública de Mato Grosso. Mesmo em andamento, já dispõe de serviços essenciais para o dia a dia.",
+    // title: "Portal Servidor Versão Mobile",
+    // description:
+    //   "O Portal do Servidor ainda está em fase de protótipo, com novas funcionalidades sendo desenvolvidas para melhorar a experiência dos defensores e servidores da Defensoria Pública de Mato Grosso. Mesmo em andamento, já dispõe de serviços essenciais para o dia a dia.",
     image: "/images/PortServidor.png",
-    details: [
-      "Visualização de peticionamentos",
-      "Audiências agendadas",
-      "Vade Mecum",
-      "Avisos pendentes",
-      "Dúvidas do Jurídico ",
-      "Ementário",
-    ],
-    gallery: ["images/BannerMobile.png"],
+    // details: [
+    //   "Visualização de peticionamentos",
+    //   "Audiências agendadas",
+    //   "Vade Mecum",
+    //   "Avisos pendentes",
+    //   "Dúvidas do Jurídico ",
+    //   "Ementário",
+    // ],
+    // gallery: ["images/BannerMobile.png"],
   },
   {
     id: 4,
-    title: "CloudSense",
-    description:
-      "Sistema interno para gestão de recursos humanos e processos administrativos",
+    // title: "CloudSense",
+    // description:
+    //   "Sistema interno para gestão de recursos humanos e processos administrativos",
     image: "/images/CloudSense02.png",
-    details: [
-      "Gestão de documentos",
-      "Workflow automatizado",
-      "Relatórios em tempo real",
-      "Integração com sistemas legados",
-    ],
-    gallery: ["/images/cloudsense.png"],
+    // details: [
+    //   "Gestão de documentos",
+    //   "Workflow automatizado",
+    //   "Relatórios em tempo real",
+    //   "Integração com sistemas legados",
+    // ],
+    // gallery: ["/images/cloudsense.png"],
   },
   {
     id: 5,
-    title: "Portal Servidor Versão Web",
-    description:
-      "A versão web do Portal do Servidor oferece ainda mais ferramentas para os defensores e servidores da Defensoria Pública de Mato Grosso. Além dos serviços já disponíveis na versão mobile, a plataforma para desktop conta com inteligência artificial para consultas jurídicas e um painel com as principais notícias internas.",
+    // title: "Portal Servidor Versão Web",
+    // description:
+    //   "A versão web do Portal do Servidor oferece ainda mais ferramentas para os defensores e servidores da Defensoria Pública de Mato Grosso. Além dos serviços já disponíveis na versão mobile, a plataforma para desktop conta com inteligência artificial para consultas jurídicas e um painel com as principais notícias internas.",
     image: "/images/LaptopServidor.png",
-    details: [
-      "Visualização de peticionamentos",
-      "Audiências agendadas",
-      "Vade Mecum",
-      "Avisos pendentes",
-      "Dúvidas do Jurídico ",
-      "Ementário",
-      "Inteligência Artificial",
-      "Notícias Internas",
-    ],
-    gallery: ["images/HomeServidor.png"],
+    // details: [
+    //   "Visualização de peticionamentos",
+    //   "Audiências agendadas",
+    //   "Vade Mecum",
+    //   "Avisos pendentes",
+    //   "Dúvidas do Jurídico ",
+    //   "Ementário",
+    //   "Inteligência Artificial",
+    //   "Notícias Internas",
+    // ],
+    // gallery: ["images/HomeServidor.png"],
   },
   {
     id: 6,
-    title: "Portal Servidor",
-    description:
-      "Sistema interno para gestão de recursos humanos e processos administrativos",
+    // title: "Portal Servidor",
+    // description:
+    //   "Sistema interno para gestão de recursos humanos e processos administrativos",
     image: "/images/Savitar.png",
-    details: [
-      "Gestão de documentos",
-      "Workflow automatizado",
-      "Relatórios em tempo real",
-      "Integração com sistemas legados",
-    ],
-    gallery: [
-      "https://images.unsplash.com/photo-1551434678-e076c223a692?auto=format&fit=crop&w=800&q=80",
-      "https://images.unsplash.com/photo-1551434678-e076c223a692?auto=format&fit=crop&w=800&q=80",
-    ],
+    // details: [
+    //   "Gestão de documentos",
+    //   "Workflow automatizado",
+    //   "Relatórios em tempo real",
+    //   "Integração com sistemas legados",
+    // ],
+    // gallery: ["images/PortalServidor.png"],
   },
 ];
 
@@ -152,7 +149,7 @@ function Drawer() {
             >
               <img
                 src={project.image}
-                alt={project.title}
+                // alt={project.title}
                 className="w-full h-64 object-cover transition-transform duration-300 group-hover:scale-110"
               />
               {/* <div className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center">
@@ -180,7 +177,7 @@ function Drawer() {
               <div className="p-6">
                 <div className="flex justify-between items-center mb-6">
                   <h2 className="text-2xl font-bold">
-                    {selectedProject.title}
+                    {/* {selectedProject.title} */}
                   </h2>
                   <button
                     onClick={() => setSelectedProject(null)}
@@ -191,18 +188,18 @@ function Drawer() {
                 </div>
 
                 <div className="space-y-6">
-                  <p className="text-gray-300">{selectedProject.description}</p>
+                  {/* <p className="text-gray-300">{selectedProject.description}</p> */}
 
-                  <div>
+                  {/* <div>
                     <h3 className="text-xl font-semibold mb-3">Detalhes</h3>
                     <ul className="list-disc list-inside space-y-2 text-gray-300">
                       {selectedProject.details.map((detail, index) => (
                         <li key={index}>{detail}</li>
                       ))}
                     </ul>
-                  </div>
+                  </div> */}
 
-                  <div>
+                  {/* <div>
                     <h3 className="text-xl font-semibold mb-3">Galeria</h3>
                     <div className="grid grid-cols-1 gap-4">
                       {selectedProject.gallery.map((image, index) => (
@@ -214,7 +211,7 @@ function Drawer() {
                         />
                       ))}
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
